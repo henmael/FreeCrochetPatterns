1acab812a0a27cb18b2a7306e4356bea673f076b
Henriette <henelt@outlook.com>
2026-02-24 00:46:06 +0100

    add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,18 @@ import { loadCrochetData } from './helper/loadCrochetPatterns'
 import { CrochetContext } from './helper/CrochetContext'
 import { CrochetPattern } from './CrochetGallery/crochetPattern'
 import InstagramIcon from '@mui/icons-material/Instagram';
+import { useEffect, useMemo, useRef, useState } from 'react'
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(8);
+
+  const data = loadCrochetData();
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+
+  const hasMore = indexOfLastItem < data.length
 
   return (
     <div className='flex flex-col h-screen justify-between'>
@@ -17,12 +27,15 @@ function App() {
                   Free Crochet Patterns
             </h1>
         </div>
-          <CrochetContext.Provider value={loadCrochetData()}>
+        <CrochetContext.Provider value={data.slice(0, indexOfLastItem)}>
           <Routes>
             <Route path="/" element={<CrochetGallery/>}/>
             <Route path="/pattern/:id" element={<CrochetPattern/>}/>
           </Routes>
           </CrochetContext.Provider>
+          <div className={`text-center text-bold text-lg transform transition hover:scale-110 ${(hasMore) ? 'opacity-100' : 'text-gray opacity-40'}`}>
+            <button className={`border-3 border-solid p-2 rounded-md`} onClick={() => setCurrentPage(currentPage+1)} disabled={!hasMore}>Load More</button>
+          </div>
       </HashRouter>
       <footer className='text-center content-bottom text-xl m-4'>
         <a href='https://www.instagram.com/henriette_crochet/' target='_blank' rel="noopener noreferrer"><InstagramIcon style={{fontSize: '2.5em'}}/></a>
