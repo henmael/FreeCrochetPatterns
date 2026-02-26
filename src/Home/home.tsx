5cd57d6b71d3973042b48adf90dd382b9191160f
Henriette <henelt@outlook.com>
2026-02-26 16:06:33 +0100

    add home text

---
+++ src/Home/home.tsx
@@ -0,0 +1,17 @@
+
+export function Home(){
+
+
+    return (
+        <div className="absolute top-50 left-20 mt-20">
+            <div className="absolute w-100 border-3 border-[#A5B452] rounded-md p-2">
+                <h1 className="text-3xl top-0 left-0">What is this website for?</h1>
+                <p className="mt-3 text-xl">This website was made to display free crochet patterns which henriette_crochet
+                    have tried out, and posted on her instagram account. 
+                    The websites goal is to make it easier for people interested in making the crochet 
+                    decorations or clothes found on henriette_crochet can easily do so through the website.
+                </p>
+            </div>
+        </div>
+    )
+}
