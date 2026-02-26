667183f2fbad5012640e0c4b38790cf064a15a79
Henriette <henelt@outlook.com>
2026-02-26 15:58:48 +0100

    add tabs for home and gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,40 @@
 import './App.css'
 import { CrochetGallery } from './CrochetGallery/crochetGallery'
-import { HashRouter, NavLink, Route, Routes } from 'react-router-dom'
+import { HashRouter, NavLink, Route, Routes, useLocation } from 'react-router-dom'
 import { loadCrochetData } from './helper/loadCrochetPatterns'
 import { CrochetContext } from './helper/CrochetContext'
 import { CrochetPattern } from './CrochetGallery/crochetPattern'
 import InstagramIcon from '@mui/icons-material/Instagram';
+import { Home } from './Home/home'
+import { colors, Tab, Tabs } from '@mui/material'
+import { useState } from 'react'
 function App() {
 
   const data = loadCrochetData()
 
   const date = new Date();
 
+  const location = useLocation();
+
   return (
     <div className='flex flex-col h-screen justify-between'>
-      <HashRouter>
         <div className='flex mt-5 text-center'>
             <NavLink to="/"><img title='Home' className='w-15 h-15 sm:w-25 sm:h-25 flex-1 mt-6 hover:scale-125 transform transition hover:opacity-65' src='cat.png' alt=""/></NavLink>
             <h1 className='text-2xl sm:text-8xl flex-1 text-center'>
                   Free Crochet Patterns
             </h1>
         </div>
+        <Tabs value={location.pathname} sx={{position: 'absolute', marginTop: 18, marginLeft: 1, ".Mui-selected": {color: `white`}, '. MuiTabs-indicator': {backgroundColor: 'white'}}} >
+          <Tab sx={{color: 'white'}} label='Home' value='/' component={NavLink} to='/'/>
+          <Tab sx={{color: 'white'}} label='Gallery' value='/gallery' component={NavLink} to='/gallery' />
+        </Tabs>
         <CrochetContext.Provider value={data}>
           <Routes>
-            <Route path="/" element={<CrochetGallery/>}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/gallery" element={<CrochetGallery/>}/>
             <Route path="/pattern/:id" element={<CrochetPattern/>}/>
           </Routes>
           </CrochetContext.Provider>
-      </HashRouter>
       <footer className='text-center content-bottom text-xl m-4'>
         <a href='https://www.instagram.com/henriette_crochet/' target='_blank' rel="noopener noreferrer"><InstagramIcon style={{fontSize: '2.5em'}}/></a>
         <p>© {date.getFullYear()}; Designed by henriette_crochet</p>
--- src/CrochetGallery/crochetGallery.tsx
+++ src/CrochetGallery/crochetGallery.tsx
@@ -9,7 +9,7 @@ export function CrochetGallery(){
     return
 
     return (
-        <div className="grid md:grid-cols-4 sm:ml-4 sm:mr-4 gap-8 mb-10 sm:mt-10 justify-items-center text-center">
+        <div className="grid md:grid-cols-4 sm:ml-4 sm:mr-4 gap-8 mb-10 sm:mt-10 justify-items-center text-center md:mt-20 mt-20">
             {data.map(cp => (
               <div key={cp.id} className="hover:scale-110 transform transition">
                 <div title={`${cp.pattern_name}`} key={cp.id} className="sm:w-92 sm:h-92 w-47 h-47 flex flex-col
